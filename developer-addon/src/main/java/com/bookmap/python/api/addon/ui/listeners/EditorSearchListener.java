361a2d9b22645f3fb1a2912909a64c0d0d65078d
romanp <romanp@bookmap.com>
2023-09-26 10:52:47 +0300

    Create ctrl+f, ctrl+h key-binds to find/replace actions

---
+++ developer-addon/src/main/java/com/bookmap/python/api/addon/ui/listeners/EditorSearchListener.java
@@ -0,0 +1,54 @@
+package com.bookmap.python.api.addon.ui.listeners;
+
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+import org.fife.rsta.ui.search.SearchEvent;
+import org.fife.rsta.ui.search.SearchListener;
+import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
+import org.fife.ui.rtextarea.SearchContext;
+import org.fife.ui.rtextarea.SearchEngine;
+import org.fife.ui.rtextarea.SearchResult;
+
+public class EditorSearchListener implements SearchListener {
+
+    private final RSyntaxTextArea textArea;
+
+    public EditorSearchListener(RSyntaxTextArea textArea) {
+        this.textArea = textArea;
+    }
+
+    @Override
+    public void searchEvent(SearchEvent e) {
+        SearchEvent.Type type = e.getType();
+        SearchContext context = e.getSearchContext();
+        SearchResult result;
+
+        switch (type) {
+            default: // Prevent FindBugs warning later
+            case MARK_ALL:
+                result = SearchEngine.markAll(textArea, context);
+                break;
+            case FIND:
+                result = SearchEngine.find(textArea, context);
+                if (!result.wasFound() || result.isWrapped()) {
+                    UIManager.getLookAndFeel().provideErrorFeedback(textArea);
+                }
+                break;
+            case REPLACE:
+                result = SearchEngine.replace(textArea, context);
+                if (!result.wasFound() || result.isWrapped()) {
+                    UIManager.getLookAndFeel().provideErrorFeedback(textArea);
+                }
+                break;
+            case REPLACE_ALL:
+                result = SearchEngine.replaceAll(textArea, context);
+                JOptionPane.showMessageDialog(null, result.getCount() + " occurrences replaced.");
+                break;
+        }
+    }
+
+    @Override
+    public String getSelectedText() {
+        return textArea.getSelectedText();
+    }
+}
